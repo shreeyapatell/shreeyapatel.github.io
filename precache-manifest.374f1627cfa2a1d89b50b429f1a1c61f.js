fcf163557af192d1ed42d3d34249e3b28d88e3bd
Shreeya Patel <shreeyapaatel@gmail.com>
2022-10-04 21:58:09 -0400

    Updates

---
+++ precache-manifest.374f1627cfa2a1d89b50b429f1a1c61f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae22f94b56c53ba5d193",
+    "url": "/website/static/css/main.1144562e.chunk.css"
+  },
+  {
+    "revision": "ae22f94b56c53ba5d193",
+    "url": "/website/static/js/main.ae22f94b.chunk.js"
+  },
+  {
+    "revision": "6f8fb3f68196abee84b3",
+    "url": "/website/static/js/1.6f8fb3f6.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "707e57f3abb846d28d60f07f5564ff07",
+    "url": "/website/static/media/about.707e57f3.jpg"
+  },
+  {
+    "revision": "a56a5bbaf1568f7c9bae7998b4646c42",
+    "url": "/website/static/media/Shreeya_Patel_CV_.a56a5bba.pdf"
+  },
+  {
+    "revision": "191feb907d94a0993009182be5d1418a",
+    "url": "/website/static/media/work1.191feb90.jpg"
+  },
+  {
+    "revision": "b855399263e7984f81521116704125b1",
+    "url": "/website/static/media/work2.b8553992.jpg"
+  },
+  {
+    "revision": "27dc07a4f0d8b0cc088598f58cd3d94e",
+    "url": "/website/static/media/work3.27dc07a4.jpg"
+  },
+  {
+    "revision": "09b0176f1df222939a6e3a48454a43db",
+    "url": "/website/static/media/work4.09b0176f.jpg"
+  },
+  {
+    "revision": "0087a913655292fe673c3b2e5dce289f",
+    "url": "/website/static/media/work5.0087a913.jpg"
+  },
+  {
+    "revision": "ac4e5bfe95d2109bff02498797ecae02",
+    "url": "/website/static/media/profile.ac4e5bfe.jpg"
+  },
+  {
+    "revision": "8237600410d3770045a5d103268f8ff5",
+    "url": "/website/index.html"
+  }
+];
