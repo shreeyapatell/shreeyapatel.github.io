e3c1df6e60aa9cdbde327bd62cabd702d79570a4
Shreeya Patel <shreeyapaatel@gmail.com>
2022-10-06 19:54:03 -0400

    projects images added

---
+++ src/components/work/Data.js
@@ -0,0 +1,53 @@
+import Work1 from '../../assets/work1.jpg'
+import Work2 from '../../assets/work2.jpg'
+import Work3 from '../../assets/work3.jpg'
+import Work4 from '../../assets/work4.jpg'
+import Work5 from '../../assets/work5.jpg'
+
+export const projectsData = [
+    {
+        id: 1, 
+        image: Work1,
+        title: 'Running Record, Amira Learning',
+        category: 'web',
+    },
+    {
+        id: 2, 
+        image: Work2,
+        title: 'Eco Advisor, Blackwings Hackathon',
+        category: 'web',
+    },
+    {
+        id: 3, 
+        image: Work3,
+        title: 'Alpha Learning Series, Children Books',
+        category: 'design',
+    },
+    {
+        id: 4, 
+        image: Work4,
+        title: '"Tiktok" for Business',
+        category: 'app',
+    },
+    {
+        id: 5, 
+        image: Work5,
+        title: 'Delivery App, Cheers',
+        category: 'design',
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design',
+    },
+]
